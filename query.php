2fa11f8f4937a29c41518924e947c1a753285129
chaosannals <chaosannals@outlook.com>
2021-05-31 10:53:12 +0800

    2.0.8

--- query.php
+++ query.php
@@ -5,7 +5,7 @@ use exert\RedisearchQuery;
 
 require __DIR__ . '/vendor/autoload.php';
 
-$query = new RedisearchQuery("@body:中文 @title:2");
+$query = new RedisearchQuery("@body:中文标题 | @title:2");
 $client = new RedisearchClient('127.0.0.1', 16379);
 $client->connect();
 $r = $client->query('myIdx', $query);
